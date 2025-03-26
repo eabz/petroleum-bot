13da0cf1b5cf0437c3299087599960cc32e3ed75
eabz <eabz@kindynos.mx>
2025-03-26 13:15:26 -0600

    Fix

--- index.ts
+++ index.ts
@@ -1,18 +1,19 @@
 import 'dotenv/config'
-import { createPublicClient, createWalletClient, getContract, http, type Address } from 'viem'
+import { http, type Address, createPublicClient, createWalletClient } from 'viem'
+import { privateKeyToAccount } from 'viem/accounts'
+import { waitForTransactionReceipt } from 'viem/actions'
 import { sonic } from 'viem/chains'
 import { REWARDS_MANAGER_ABI } from './abis'
-import { CRUDE_OIL_TOKEN, MAX_REWARD_TO_CLAIM, PLOT_ID, REFINERY_ADDRESS, REWARDS_MANAGER_ADDRESS } from './constants'
-import { privateKeyToAccount } from 'viem/accounts'
 import { ERC20_ABI } from './abis/erc20'
 import { REFINERY_ABI } from './abis/refinery'
+import { CRUDE_OIL_TOKEN, MAX_REWARD_TO_CLAIM, PLOT_ID, REFINERY_ADDRESS, REWARDS_MANAGER_ADDRESS } from './constants'
 
-export const publicClient = createPublicClient({
+export const PUBLIC_CLIENT = createPublicClient({
   chain: sonic,
   transport: http()
 })
 
-export const walletClient = createWalletClient({ chain: sonic, transport: http() })
+export const WALLET_CLIENT = createWalletClient({ chain: sonic, transport: http() })
 
 async function main() {
   console.log('==> Starting Petroleum Bot')
@@ -29,7 +30,7 @@ async function main() {
 
   console.log('==> Checking available reward')
 
-  const reward = (await publicClient.readContract({
+  const reward = (await PUBLIC_CLIENT.readContract({
     address: REWARDS_MANAGER_ADDRESS,
     abi: REWARDS_MANAGER_ABI,
     functionName: 'getRewardOfPlot',
@@ -39,18 +40,20 @@ async function main() {
   if (reward >= MAX_REWARD_TO_CLAIM) {
     console.log('==> Claiming rewards')
 
-    await walletClient.writeContract({
+    const tx = await WALLET_CLIENT.writeContract({
       account,
       address: REWARDS_MANAGER_ADDRESS,
       abi: REWARDS_MANAGER_ABI,
       functionName: 'claimRewardOfPlot',
       args: [PLOT_ID]
     })
+
+    await waitForTransactionReceipt(PUBLIC_CLIENT, { hash: tx })
   }
 
   console.log('==> Getting the balance of cOIL')
 
-  const balance = (await publicClient.readContract({
+  const balance = (await PUBLIC_CLIENT.readContract({
     address: CRUDE_OIL_TOKEN,
     abi: ERC20_ABI,
     functionName: 'balanceOf',
@@ -60,7 +63,7 @@ async function main() {
   if (balance >= MAX_REWARD_TO_CLAIM) {
     console.log('==> Getting list of decayed nodes')
 
-    const decayedNodes = (await publicClient.readContract({
+    const decayedNodes = (await PUBLIC_CLIENT.readContract({
       address: REWARDS_MANAGER_ADDRESS,
       abi: REWARDS_MANAGER_ABI,
       functionName: 'getAllDecayedNodeOfPlot',
@@ -70,7 +73,7 @@ async function main() {
     if (decayedNodes.length > 0) {
       console.log('==> Fixing one node')
 
-      await walletClient.writeContract({
+      const tx = await WALLET_CLIENT.writeContract({
         account,
         address: REWARDS_MANAGER_ADDRESS,
         abi: REWARDS_MANAGER_ABI,
@@ -78,26 +81,30 @@ async function main() {
         args: [decayedNodes[0], CRUDE_OIL_TOKEN]
       })
 
+      await waitForTransactionReceipt(PUBLIC_CLIENT, { hash: tx })
+
       console.log('==> Sending left balance to refinery')
 
-      const leftBalance = (await publicClient.readContract({
+      const leftBalance = (await PUBLIC_CLIENT.readContract({
         address: CRUDE_OIL_TOKEN,
         abi: ERC20_ABI,
         functionName: 'balanceOf',
         args: [account.address]
       })) as bigint
 
-      await walletClient.writeContract({
+      const tx2 = await WALLET_CLIENT.writeContract({
         account,
         address: REFINERY_ADDRESS,
         abi: REFINERY_ABI,
         functionName: 'refinate',
         args: [leftBalance]
       })
+
+      await waitForTransactionReceipt(PUBLIC_CLIENT, { hash: tx2 })
     }
   }
 
   console.log('==> Finished')
 }
 
-setInterval(main, 1000 * 60)
+setInterval(main, 1000 * 30)
